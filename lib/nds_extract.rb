5aba2ff0444dc880d131810ea68dbbafe46b9d9e
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-30 01:37:24 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,11 +5,13 @@ require 'directors_database'
 def gross_for_director(director_data)
   movie_index = 0
   movie_total = 0
-  while movie_index < director_data[:movies].length do
+
+  while movie_index < director_data[:movies].length do #director_data = gross_for_director(nds[start])
     movie_total += director_data[:movies][movie_index][:worldwide_gross]
     movie_index += 1
   end
-  movie_total
+
+  movie_total #returning movie_total to function gross_for_director
 end
 
 
@@ -17,7 +19,6 @@ def directors_totals(nds)
   result = {}
 
   start = 0 #row_index
-
   while start < nds.length do
     result[nds[start][:name]] = gross_for_director(nds[start])
     start += 1
